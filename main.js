792c2575d71c71e6cf50834e20e54737ad9247e5
=ahmedyasser <=ahmedya3ser8@gmail.com>
2023-04-12 07:00:45 +0200

    Create Scroll To Top

--- main.js
+++ main.js
@@ -61,3 +61,25 @@ let count = setInterval(() => {
     clearInterval(count);
   }
 }, 1000);
+
+// Scroll To Top
+
+let scrollToTop = document.querySelector(".scroll-to-top");
+let btn = document.querySelector(".up");
+
+document.addEventListener("scroll", function() {
+    if (window.scrollY >= 600) {
+      scrollToTop.style.opacity = "1";
+      scrollToTop.style.display = "block";
+    } else {
+      scrollToTop.style.opacity = "0";
+      scrollToTop.style.display = "none";
+    }
+})
+
+btn.onclick = function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
